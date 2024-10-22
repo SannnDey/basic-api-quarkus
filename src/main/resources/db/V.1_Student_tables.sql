1bc3b7ed5bb41e26fce025dc2f53336b964c83a1
suryajayantara <ptsuryajayantara01@gmail.com>
2024-10-22 10:59:48 +0800

    :smile: add migration

---
+++ src/main/resources/db/V.1_Student_tables.sql
@@ -0,0 +1,22 @@
+create table student
+(
+    student_id  varchar(36)                        not null
+        primary key,
+    name        create table student
+(
+    student_id  varchar(36)                        not null
+        primary key,
+    name        varchar(150)                       not null,
+    identity_id varchar(20)                        not null,
+    created_at  datetime default CURRENT_TIMESTAMP not null,
+    updated_at  datetime default CURRENT_TIMESTAMP not null,
+    deleted_at  datetime                           null
+);
+
+varchar(150)                       not null,
+    identity_id varchar(20)                        not null,
+    created_at  datetime default CURRENT_TIMESTAMP not null,
+    updated_at  datetime default CURRENT_TIMESTAMP not null,
+    deleted_at  datetime                           null
+);
+
